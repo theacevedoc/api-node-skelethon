43275e7691de99e20255169e5e0c403c13203f3b
harper9204 <carlos.acevedo9204@gmail.com>
2023-03-17 01:22:48 -0600

    chore: Constants file was rename to enable es6 modules

---
+++ src/utils/lib/constants.mjs
@@ -0,0 +1,10 @@
+// responses
+export const STATUS_200 = 200;
+export const STATUS_201 = 201;
+export const STATUS_400 = 400;
+export const STATUS_500 = 500;
+export const CODE_OK = 'OK';
+export const CODE_OK_CREATED = 'OK_CREATED';
+export const CODE_ZERO_RESULTS = 'ZERO_RESULTS';
+export const CODE_ERROR_PARAMS = 'ERROR_PARAMS';
+export const CODE_ERROR = 'ERROR'
