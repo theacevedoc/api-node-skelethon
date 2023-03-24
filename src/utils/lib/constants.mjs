67467ba473a86d3873937ea0e3a67392d0abb832
harper9204 <carlos.acevedo9204@gmail.com>
2023-03-23 23:38:01 -0600

    chore: Add message error to constants

--- src/utils/lib/constants.mjs
+++ src/utils/lib/constants.mjs
@@ -8,3 +8,4 @@ export const CODE_OK_CREATED = 'OK_CREATED';
 export const CODE_ZERO_RESULTS = 'ZERO_RESULTS';
 export const CODE_ERROR_PARAMS = 'ERROR_PARAMS';
 export const CODE_ERROR = 'ERROR'
+export const MESSAGE_ERROR = 'Something went wrong';
